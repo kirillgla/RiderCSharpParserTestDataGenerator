fb58b80970b7ed05e6289da41f1cb21b1f4e83be
Kirill Glazyrin <Kirill.Glazyrin@jetbrains.com>
2021-08-23 21:26:03 -0700

    Initial commit

---
+++ TestDataGenerator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TestDataGenerator
+{
+    internal static class Program
+    {
+        private const string TestDataLocation =
+            @"C:\w\dotnet-products\Rider\Frontend\rider-test-cases\testData\psi\parsing\CSharpDummyParserConcatenationTest";
+
+        private static void Main()
+        {
+            GenerateTestData();
+        }
+
+        private static void GenerateTestData()
+        {
+            int currentIndex = 0;
+            foreach (var leftParameter in Enum.GetValues<StringKind>())
+            {
+                foreach (var rightParameter in Enum.GetValues<StringKind>())
+                {
+                    string fileName = $"concatenation of interpolated strings {currentIndex:00}.txt";
+                    string contents = $@"CSharpFile
+  CSharpDummyNode
+{leftParameter.GetDumpedPsi()}
+    PsiWhiteSpace(' ')
+    PsiElement(PLUS)('+')
+    PsiWhiteSpace(' ')
+{rightParameter.GetDumpedPsi()}";
+                    File.WriteAllText($"{TestDataLocation}\\{fileName}", contents);
+                    currentIndex += 1;
+                }
+            }
+        }
+
+        private static void GenerateTestSources()
+        {
+            int currentIndex = 0;
+            foreach (var leftParameter in Enum.GetValues<StringKind>())
+            {
+                foreach (var rightParameter in Enum.GetValues<StringKind>())
+                {
+                    string fileName = $"concatenation of interpolated strings {currentIndex:00}.cs";
+                    string contents = $"{leftParameter.GetCodeExample()} + {rightParameter.GetCodeExample()}";
+                    File.WriteAllText($"{TestDataLocation}\\{fileName}", contents);
+                    currentIndex += 1;
+                }
+            }
+        }
+
+        private static IEnumerable<string> GenerateMethodNames()
+        {
+            int currentIndex = 0;
+            foreach (var leftParameter in Enum.GetValues<StringKind>())
+            {
+                foreach (var rightParameter in Enum.GetValues<StringKind>())
+                {
+                    yield return
+                        $"fun `test concatenation of interpolated strings {currentIndex:00}`() = doTest() // " +
+                        $"{leftParameter.GetCommentPresentation()} + {rightParameter.GetCommentPresentation()}";
+                    currentIndex += 1;
+                }
+            }
+        }
+    }
+}
---
+++ TestDataGenerator/StringKind.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TestDataGenerator
+{
+    public enum StringKind
+    {
+        Regular,
+        Interpolated,
+        Verbatim,
+        VerbatimInterpolated,
+        InterpolatedWithInsertion,
+        VerbatimInterpolatedWithInsertion
+    }
+
+    public static class StringKindExtensions
+    {
+        public static string GetCommentPresentation(this StringKind stringKind) => stringKind switch
+        {
+            StringKind.Regular => "regular",
+            StringKind.Interpolated => "interpolated",
+            StringKind.Verbatim => "verbatim",
+            StringKind.VerbatimInterpolated => "verbatim interpolated",
+            StringKind.InterpolatedWithInsertion => "interpolated with insertion",
+            StringKind.VerbatimInterpolatedWithInsertion => "verbatim interpolated with insertion",
+            _ => throw new ArgumentOutOfRangeException(nameof(stringKind), stringKind, null)
+        };
+
+        public static string GetCodeExample(this StringKind stringKind) => stringKind switch
+        {
+            StringKind.Regular => "\"\"",
+            StringKind.Interpolated => "$\"\"",
+            StringKind.Verbatim => "@\"\"",
+            StringKind.VerbatimInterpolated => "$@\"\"",
+            StringKind.InterpolatedWithInsertion => "$\"{x}\"",
+            StringKind.VerbatimInterpolatedWithInsertion => "$@\"{x}\"",
+            _ => throw new ArgumentOutOfRangeException(nameof(stringKind), stringKind, null)
+        };
+
+        public static string GetDumpedPsi(this StringKind stringKind) => stringKind switch
+        {
+            StringKind.Regular => @"    CSharpNonInterpolatedStringLiteralExpression
+      PsiElement(STRING_LITERAL_REGULAR)('""""')",
+            StringKind.Interpolated => @"    CSharpInterpolatedStringLiteralExpression
+      CSharpInterpolatedStringLiteralExpressionPart
+        PsiElement(INTERPOLATED_STRING_REGULAR)('$""""')",
+            StringKind.Verbatim => @"    CSharpNonInterpolatedStringLiteralExpression
+      PsiElement(STRING_LITERAL_VERBATIM)('@""""')",
+            StringKind.VerbatimInterpolated => @"    CSharpInterpolatedStringLiteralExpression
+      CSharpInterpolatedStringLiteralExpressionPart
+        PsiElement(INTERPOLATED_STRING_VERBATIM)('$@""""')",
+            StringKind.InterpolatedWithInsertion => @"    CSharpInterpolatedStringLiteralExpression
+      CSharpInterpolatedStringLiteralExpressionPart
+        PsiElement(INTERPOLATED_STRING_REGULAR_START)('$""{')
+      PsiElement(IDENTIFIER)('x')
+      CSharpInterpolatedStringLiteralExpressionPart
+        PsiElement(INTERPOLATED_STRING_REGULAR_END)('}""')",
+            StringKind.VerbatimInterpolatedWithInsertion => @"    CSharpInterpolatedStringLiteralExpression
+      CSharpInterpolatedStringLiteralExpressionPart
+        PsiElement(INTERPOLATED_STRING_VERBATIM_START)('$@""{')
+      PsiElement(IDENTIFIER)('x')
+      CSharpInterpolatedStringLiteralExpressionPart
+        PsiElement(INTERPOLATED_STRING_VERBATIM_END)('}""')",
+            _ => throw new ArgumentOutOfRangeException(nameof(stringKind), stringKind, null)
+        };
+    }
+}
